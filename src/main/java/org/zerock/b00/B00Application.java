4034e964960404d21557d366308c6253e7dbc8df
zerock <zkang075@gmail.com>
2021-09-18 19:51:51 +0900

    config1

---
+++ src/main/java/org/zerock/b00/B00Application.java
@@ -0,0 +1,13 @@
+package org.zerock.b00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B00Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/b00/B00ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.b00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
